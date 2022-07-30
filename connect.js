6398a5b85be37524b48f89fd5691532a77e1c2b7
Shrishty <shrishhty@gmail.com>
2022-07-30 17:46:05 +0530

    Connecting Smart Contract

--- connect.js
+++ connect.js
@@ -8,7 +8,7 @@ const connect = new Promise((res, rej) => {
     window.ethereum.request({method: "eth_requestAccounts" });
 
     let web3 = new Web3(window.ethereum);
-    let contract = new web3.eth.Contract(abi, "0x66b3fd5f78f09718ed12807cf3A2CA717C97264f");
+    let contract = new web3.eth.Contract(abi, "0x8e3C79689c51023852D8A62Da64B2dE998Ef30FD");
 
     web3.eth.getAccounts().then((accounts) => {
         contract.methods.totalSupply().call({from: accounts[0]}).then((supply) => {
